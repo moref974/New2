89e52cad0c2743619305be35ea96fd461e39e0ba
yuri <moref974@gmail.com>
2025-03-28 21:24:25 +0500

    new

--- app/api/webhook/route.js
+++ app/api/webhook/route.js
@@ -2,7 +2,7 @@ import { NextResponse } from "next/server"
 
 export async function POST(req) {
   try {
-    console.log("WebHook Recieved ")
+    console.log("WebHook Recieved Done")
     const body = await req.json()
     console.log("📩 New Event Received:", body)
 
@@ -11,8 +11,7 @@ export async function POST(req) {
     return NextResponse.json({ success: false, error: error.message }, { status: 500 })
   }
 }
-
-export async function Get(req) {
+export async function GET(req) {
   return NextResponse.json(
     {
       message: "webhook is working, but use POST request",
